eda6dab4697aced15160171979c8630130c3a3ef
Cristian García <cristiangarcia@iespaucasesnoves.cat>
2019-02-19 18:47:45 +0100

    Codigo implementado, pero sin saber si funciona.

--- app/src/main/java/com/example/cristiangarcia/chat/MainActivity.java
+++ app/src/main/java/com/example/cristiangarcia/chat/MainActivity.java
@@ -2,12 +2,103 @@ package com.example.cristiangarcia.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText nombre, password;
+    String url = "http://iesmantpc.000webhostapp.com/public/login/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.login_btn);
+        nombre = findViewById(R.id.userInput);
+        password = findViewById(R.id.passInput);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put(nombre.getText().toString(), password.getText().toString());
+                CridadaPost(url, hashMap);
+            }
+        });
+    }
+
+    public static String CridadaPost(String adrecaURL, HashMap<String,
+            String> parametres) {
+        String resultat = "";
+        try {
+            URL url = new URL(adrecaURL);
+            Log.i("ResConnectUtils", "Connectant" + adrecaURL);
+            HttpsURLConnection httpConn = (HttpsURLConnection) url.openConnection();
+            httpConn.setReadTimeout(15000);
+            httpConn.setConnectTimeout(25000);
+            httpConn.setRequestMethod("POST");
+            httpConn.setDoInput(true);
+            httpConn.setDoOutput(true);
+            OutputStream os = httpConn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(montaParametres(parametres));
+            writer.flush();
+            writer.close();
+            os.close();
+            int resposta = httpConn.getResponseCode();
+            if (resposta == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new
+                        InputStreamReader(httpConn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    resultat += line;
+                }
+                Log.i("ResConnectUtils", resultat);
+            } else {
+                resultat = "";
+                Log.i("ResConnectUtils", "Errors:" + resposta);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return resultat;
+    }
+
+    private static String montaParametres(HashMap<String, String> params) throws
+            UnsupportedEncodingException {
+        // A partir d'un hashmap clau-valor cream
+        // clau1=valor1&clau2=valor2&...
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (first) {
+                first = false;
+            } else {
+                result.append("&");
+            }
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
     }
 }
