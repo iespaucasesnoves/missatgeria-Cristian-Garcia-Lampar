c63b3c39285b672b3d4873f20fd32979188e7f2a
Cristian García <cristiangarcia@iespaucasesnoves.cat>
2019-02-19 19:14:49 +0100

    Me he dado cuenta de que los metodos proporcionados tienen que estar en una clase extendida a AsyncTask. Tambien he metido en el manifest los permisos de internet y de usar protocolos sin seguridad.

---
+++ app/src/main/java/com/example/cristiangarcia/chat/Login.java
@@ -0,0 +1,10 @@
+package com.example.cristiangarcia.chat;
+
+import android.os.AsyncTask;
+
+public class Login extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
